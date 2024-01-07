a4c45c02d19abefaac06325d7d96d22ac81650a9
Juan Ricardo Deossa <110882230+JuanDeossa@users.noreply.github.com>
2024-01-07 01:03:17 +0000

    se agrega una gráfica de prueba

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,34 @@
 import { Box, Button, Paper, Stack } from "@mui/material";
+import { Chart } from "../components/chart/Chart";
+import { BasicTable } from "../components/basicTable/BasicTable";
+import { useState } from "react";
 
 export const MainPage = () => {
+  const [constants, setConstants] = useState({
+    D: 0.1776,
+    L: 750,
+    C: 110,
+    R: 5994.862241,
+  });
+
   return (
-    <Stack direction="column" alignItems="center" spacing={3} paddingTop={4} width={390} border="1px solid gray" borderBottom="0px">
+    <Stack
+      direction="column"
+      alignItems="center"
+      spacing={5}
+      paddingTop={4}
+      width={390}
+      border="1px solid gray"
+      borderBottom="0px"
+    >
       <Paper elevation={4}>
-        <Box width={300} height={300}></Box>
+        <Chart />
       </Paper>
+      {/* <Paper elevation={4}>
+        <Box overflow="auto">
+          <BasicTable />
+        </Box>
+      </Paper> */}
       <Button variant="contained">Calcular</Button>
     </Stack>
   );
