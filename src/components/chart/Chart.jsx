38539d052a5a1a501b055690988e1d6dc86c6232
Juan Ricardo Deossa <110882230+JuanDeossa@users.noreply.github.com>
2024-01-09 06:27:18 +0000

    Se corrigen leyendas

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/prop-types */
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { LineChart } from "@mui/x-charts";
-import modStyles from './Chart.module.css'
-
+import modStyles from "./Chart.module.css";
 
 export const Chart = ({ data }) => {
   const { eqName, DATA_X, DATA_Y1, DATA_Y2, DATA_Y3 } = data || {};
@@ -13,7 +12,8 @@ export const Chart = ({ data }) => {
       </Typography>
       <LineChart
         width={300}
-        height={300}
+        height={260}
+        className="LineChart"
         title="Nooo"
         xAxis={[{ data: DATA_X || [0, 1, 2, 3, 4, 5], label: "Q (GPM)" }]}
         series={[
@@ -21,22 +21,37 @@ export const Chart = ({ data }) => {
             data: DATA_Y1 || [0, 1, 2, 3, 4, 5],
             color: !DATA_Y1?.length ? "transparent" : "blue",
             showMark: false,
-            label: "P (MCA)",
+            label: "Curva real de trabajo",
           },
           {
             data: DATA_Y2 || [0, 1, 2, 3, 4, 5],
             color: !DATA_Y2?.length ? "transparent" : "red",
             showMark: false,
-            label: "P (mca)",
+            label: "Curva de desempeño de fabricante",
           },
           {
             data: DATA_Y3 || [0, 1, 2, 3, 4, 5],
             color: !DATA_Y3?.length ? "transparent" : "green",
             showMark: false,
-            label: "Shaft Power (Kw)",
+            label: "Eficiencia",
           },
         ]}
       />
+      <Stack direction="column" spacing={2} paddingLeft={2} paddingTop={1}>
+        <Stack direction="row">
+          <Box width={20} height={20} bgcolor="red" marginRight={1}></Box>
+          Curva de desempeño de fabricante
+        </Stack>
+        <Stack direction="row">
+          <Box width={20} height={20} bgcolor="blue" marginRight={1}></Box>
+          Curva real de trabajo
+        </Stack>
+        <Stack direction="row">
+          <Box width={20} height={20} bgcolor="green" marginRight={1}></Box>
+          Eficiencia
+        </Stack>
+      </Stack>
+      <br></br>
     </div>
   );
 };
